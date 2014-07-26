cc3f659a2f77cfb4f8aebb8b6f8d77a8fc531470
Giridhar Bhujanga <gbhujanga@walmartlabs.com>
2014-07-26 19:29:42 +0530

    Initial Commit

---
+++ iTunesControl/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iTunesControl
+//
+//  Created by Giridhar Bhujanga on 7/26/14.
+//  Copyright (c) 2014 Walmart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
